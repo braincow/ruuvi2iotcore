efd70b62690da259db2407d092f4d96745d9a2a3
Antti Peltonen <antti.peltonen@iki.fi>
2020-11-13 18:08:43 +0200

    error handling for dns autodetection

--- src/lib/dnsconfig.rs
+++ src/lib/dnsconfig.rs
@@ -1,7 +1,33 @@
 use trust_dns_resolver::Resolver;
 use trust_dns_resolver::config::*;
-use color_eyre::eyre::Report;
+use color_eyre::{eyre::eyre, SectionExt, Section, eyre::Report};
 use std::str;
+
+fn resolve_txt_record(resolver: &Resolver, record: &String) -> Result<String, Report> {
+    match resolver.txt_lookup(record) {
+        Ok(response) => {
+            match response.iter().next() {
+                Some(response_data) => match str::from_utf8(&response_data.txt_data()[0]) {
+                    Ok(txt_str) => Ok(txt_str.to_string()),
+                    Err(error) => return Err(
+                        eyre!("Unable to parse DNS response TXT data into string.")
+                            .with_section(move || format!("{:?}", response_data).header("Byte array:"))
+                            .with_section(move || error.to_string().header("Reason:"))
+                        )            
+                },
+                None => return Err(
+                    eyre!("Empty DNS record.")
+                        .with_section(move || record.to_string().header("Record:"))
+                    )
+            }
+        },
+        Err(error) => return Err(
+            eyre!("Unable to query DNS record.")
+                .with_section(move || record.to_string().header("Record:"))
+                .with_section(move || error.to_string().header("Reason:"))
+            )
+    }
+}
 
 #[derive(Debug)]
 pub struct IotCoreConfig {
@@ -13,22 +39,19 @@ pub struct IotCoreConfig {
 
 impl IotCoreConfig {
     pub fn build(device_id: &String, domain: &String) -> Result<IotCoreConfig, Report> {
-        let resolver = Resolver::new(ResolverConfig::default(), ResolverOpts::default()).unwrap();
-
-        let project_id_response = resolver.txt_lookup(&format!("_project_id.{}", domain)).unwrap();
-        let project_id = project_id_response.iter().next().unwrap();
-
-        let region_response = resolver.txt_lookup(&format!("_region.{}", domain)).unwrap();
-        let region = region_response.iter().next().unwrap();
-
-        let registry_response = resolver.txt_lookup(&format!("_registry.{}", domain)).unwrap();
-        let registry = registry_response.iter().next().unwrap();
+        let resolver = match Resolver::new(ResolverConfig::default(), ResolverOpts::default()) {
+            Ok(resolver) => resolver,
+            Err(error) => return Err(
+                eyre!("Unable to instantiate DNS resolver.")
+                    .with_section(move || error.to_string().header("Reason:"))
+                )
+        };
 
         Ok(IotCoreConfig {
             device_id: device_id.clone(),
-            project_id: str::from_utf8(&project_id.txt_data()[0]).unwrap().to_string(),
-            region: str::from_utf8(&region.txt_data()[0]).unwrap().to_string(),
-            registry: str::from_utf8(&registry.txt_data()[0]).unwrap().to_string()
+            project_id: resolve_txt_record(&resolver, &format!("_project_id.{}", domain))?,
+            region: resolve_txt_record(&resolver, &format!("_region.{}", domain))?,
+            registry: resolve_txt_record(&resolver, &format!("_registry.{}", domain))?
         })
     }
 
